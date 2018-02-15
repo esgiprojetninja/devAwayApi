b3d61610e330b20eeec49ace62972621b476063f
GoRFy <lambot.rom@gmail.com>
2018-02-15 20:47:06 +0100

    add users api REST route

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,6 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => ['api'], 'prefix' => '/v1'], function () {
+    Route::resource('users', 'Api\v1\UserController');
 });
