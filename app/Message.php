d65b06f4e90b326038c794a2ce27369418a2be27
GoRFy <lambot.rom@gmail.com>
2018-03-04 16:57:38 +0100

    MIGRATION

---
+++ app/Message.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Message extends Model
+{
+
+    protected $table = "message";
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'id',
+        'content',
+        'accommodation',
+        'candidate',
+    ];
+
+}
