8a6de82db1e25269d795ce07498c4fe14729bc04
GoRFy <lambot.rom@gmail.com>
2018-04-18 12:10:54 +0200

    test

--- config/database.php
+++ config/database.php
@@ -7,6 +7,8 @@ $username = $url["user"];
 $password = $url["pass"];
 $database = substr($url["path"], 1);
 
+var_dump($host . " / " . $username . " / " . $password . " / " . $database );
+
 return [
 
     /*
