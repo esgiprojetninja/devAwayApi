a16a6c31e8a5400b963e595bf422d318c915271f
GoRFy <lambot.rom@gmail.com>
2018-04-18 11:07:04 +0200

    Update Heroku conf v2

--- config/database.php
+++ config/database.php
@@ -3,6 +3,8 @@
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
 $host = $url["host"];
+var_dump($host);
+die();
 $username = $url["user"];
 $password = $url["pass"];
 $database = substr($url["path"], 1);
